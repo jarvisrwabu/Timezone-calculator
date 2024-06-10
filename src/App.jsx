fa82cb9fad0034a39b68ce21615916f2aa8ffff5
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-10 19:53:51 +0300

    Testing Dnd drag and drop debugging blank render

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
-
+import { DndContext, closestCorners } from '@dnd-kit/core';
 import React from 'react'
-import Navbar from './components/Navbar'
+import Column from './column/Column'
+import "./App.css";
 
 const App = () => {
+  const [countries, setCountries] = useState([
+  {id: 1, name: "Afghanistan"},
+  {id: 2, name: "Albania"},
+  {id: 3, name: "Morocco"},
+  ]);
+
+  
   return (
     <>
-    <Navbar/>
+      <div className = "App">
+        <h1>Countries</h1>        
+      </div>
     </>
 
   )
---
+++ src/column/Column.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./column.css"
+
+
+const Column = ({countries}) => {
+  return (
+    <div className="column">Column</div>
+  )
+}
+
